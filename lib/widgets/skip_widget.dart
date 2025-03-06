851a62fb0fc22c3a1b84f2c23402f2c46e5eddf5
rabab_hamdyy <rababhamdy38@gmail.com>
2025-03-06 16:58:41 +0200

    link skip to login

--- lib/widgets/skip_widget.dart
+++ lib/widgets/skip_widget.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 class SkipWidget extends StatelessWidget {
-  const SkipWidget({
-    super.key,
-  });
+  const SkipWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -11,13 +9,18 @@ class SkipWidget extends StatelessWidget {
       padding: const EdgeInsets.fromLTRB(0, 0, 32, 0),
       child: Align(
         alignment: Alignment.topRight,
-        child: Text(
-          "Skip",
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.w400,
-            fontFamily: "Righteous",
-            color: Color(0xFF8D8D8D),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.pushNamed(context, '/login');
+          },
+          child: Text(
+            "Skip",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w400,
+              fontFamily: "Righteous",
+              color: Color(0xFF8D8D8D),
+            ),
           ),
         ),
       ),
