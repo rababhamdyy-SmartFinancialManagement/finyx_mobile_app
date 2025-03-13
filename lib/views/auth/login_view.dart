000a997729a3ee4b0cf31cce552b98ca0ed9a804
rabab_hamdyy <rababhamdy38@gmail.com>
2025-03-13 02:59:42 +0200

    add curved background widget

--- lib/views/auth/login_view.dart
+++ lib/views/auth/login_view.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Text("Welcome to Login Page"),
-      ),
-    );
+    return Scaffold(body: SafeArea(child: Center(child: Text('Login Screen'))));
   }
 }
