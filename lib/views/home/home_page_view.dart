992890e593632b018c0e05fb490385f1e638dfe5
rabab_hamdyy <rababhamdy38@gmail.com>
2025-04-11 21:38:17 +0200

    edit FAB

--- lib/views/home/home_page_view.dart
+++ lib/views/home/home_page_view.dart
@@ -1,13 +1,11 @@
 import 'package:finyx_mobile_app/cubits/bottom%20nav/navigation_cubit.dart';
 import 'package:finyx_mobile_app/cubits/bottom%20nav/navigation_state.dart';
-import 'package:finyx_mobile_app/cubits/home/chart_cubit.dart';
 import 'package:finyx_mobile_app/models/user_type.dart';
 import 'package:finyx_mobile_app/views/home/homepage_body.dart';
 import 'package:finyx_mobile_app/views/profile/profile_view.dart';
 import 'package:finyx_mobile_app/views/setting/setting_view.dart';
 import 'package:finyx_mobile_app/views/wallet/wallet_view.dart';
 import 'package:finyx_mobile_app/widgets/homepage/custom_bottom_navbar.dart';
-import 'package:finyx_mobile_app/widgets/homepage/pie_chart_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -20,8 +18,7 @@ class HomePageView extends StatelessWidget {
     return BlocBuilder<NavigationCubit, NavigationState>(
       builder: (context, state) {
         final currentIndex = state.index;
-        final screenWidth = MediaQuery.of(context).size.width;
-        final screenHeight = MediaQuery.of(context).size.height;
+        // final screenWidth = MediaQuery.of(context).size.width;
 
         return Scaffold(
           body: IndexedStack(
@@ -35,16 +32,26 @@ class HomePageView extends StatelessWidget {
           ),
           floatingActionButton: FloatingActionButton(
             onPressed: () {
-              // action for FAB
+              if (currentIndex == 1) {
+                // إذا كنا في صفحة "المحفظة"، نقوم بإضافة الوظيفة
+              
+              } else {
+                // في باقي الصفحات، نقوم بعرض الدردشة AI
+              
+              }
             },
             backgroundColor: Colors.yellow[700],
+            shape: const CircleBorder(),
             child: Padding(
               padding: EdgeInsets.all(
                 MediaQuery.of(context).size.width * 0.025,
               ),
-              child: Image.asset('assets/images/home/chatAI.png'),
+              child: Image.asset(
+                currentIndex == 1
+                    ? 'assets/images/wallet/plus.png' // إذا كنا في صفحة المحفظة
+                    : 'assets/images/home/chatAI.png', // في باقي الصفحات
+              ),
             ),
-            shape: const CircleBorder(),
           ),
           floatingActionButtonLocation:
               FloatingActionButtonLocation.centerDocked,
