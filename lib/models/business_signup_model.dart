b73147fd924539dd88db9b50f7b0c56d6bd02edc
shahd12-mohamed <shahdmohamed12122002@gmail.com>
2025-04-13 00:50:21 +0200

    model

--- lib/models/business_signup_model.dart
+++ lib/models/business_signup_model.dart
@@ -1,4 +1,6 @@
+import 'package:finyx_mobile_app/widgets/shared/custom_snack_bar_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class BusinessSignUpModel {
   final TextEditingController fullNameController = TextEditingController();
@@ -13,5 +15,33 @@ class BusinessSignUpModel {
     companyLocationController.dispose();
     budgetController.dispose();
     numberOfEmployeesController.dispose();
+  }
+
+  bool areFieldsFilled() {
+    return fullNameController.text.isNotEmpty &&
+        companyNameController.text.isNotEmpty &&
+        companyLocationController.text.isNotEmpty &&
+        budgetController.text.isNotEmpty &&
+        numberOfEmployeesController.text.isNotEmpty;
+  }
+
+  
+  Future<void> saveBusinessData(BuildContext context) async {
+    if (!areFieldsFilled()) {
+      CustomSnackbar.show(context, 'Please fill all the fields!', isError: true);
+      return; 
+    }
+
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    await prefs.setString('fullName', fullNameController.text);
+    await prefs.setString('companyName', companyNameController.text);
+    await prefs.setString('companyLocation', companyLocationController.text);
+    await prefs.setString('budget', budgetController.text);
+    await prefs.setString('numberOfEmployees', numberOfEmployeesController.text);
+
+    await prefs.setString('userType', 'business'); 
+    
+    CustomSnackbar.show(context, 'Business registration successful!');
   }
 }
--- lib/models/individual_signup_model.dart
+++ lib/models/individual_signup_model.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class IndividualSignupModel {
   final TextEditingController fullNameController = TextEditingController();
@@ -13,5 +14,26 @@ class IndividualSignupModel {
     addressController.dispose();
     incomeController.dispose();
     nationalIdController.dispose();
+  }
+
+ 
+  bool areFieldsFilled() {
+    return fullNameController.text.isNotEmpty &&
+           dobController.text.isNotEmpty &&
+           addressController.text.isNotEmpty &&
+           incomeController.text.isNotEmpty &&
+           nationalIdController.text.isNotEmpty;
+  }
+
+  Future<void> saveIndividualData(BuildContext context) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    await prefs.setString('fullName', fullNameController.text);
+    await prefs.setString('dob', dobController.text);
+    await prefs.setString('address', addressController.text);
+    await prefs.setString('income', incomeController.text);
+    await prefs.setString('nationalId', nationalIdController.text);
+
+    await prefs.setString('userType', 'individual'); 
   }
 }
