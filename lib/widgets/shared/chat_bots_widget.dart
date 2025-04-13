a86ccf5ae124d0bdc7dd83b7ef6a1a61a1c0fec2
rabab_hamdyy <rababhamdy38@gmail.com>
2025-04-13 21:05:59 +0200

    chat bots

--- lib/widgets/shared/chat_bots_widget.dart
+++ lib/widgets/shared/chat_bots_widget.dart
@@ -26,7 +26,7 @@ class _ChatDialogState extends State<ChatDialog> {
         width: dialogWidth,
         padding: EdgeInsets.all(16),
         decoration: BoxDecoration(
-          color: Color.fromARGB(255, 222, 206, 240),
+          color: Color(0xFF3E0555).withAlpha(15),
           borderRadius: BorderRadius.circular(16),
         ),
         child: Column(
@@ -34,20 +34,19 @@ class _ChatDialogState extends State<ChatDialog> {
             Align(
               alignment: Alignment.topRight,
               child: Container(
-                padding: EdgeInsets.all(6),
                 decoration: BoxDecoration(
                   color: Colors.white,
                   shape: BoxShape.circle,
                   boxShadow: [
                     BoxShadow(
-                      color: Colors.grey.withOpacity(0.3),
-                      blurRadius: 5,
+                      color: Colors.grey.withAlpha(15),
+                      blurRadius: 15,
                       offset: Offset(0, 3),
                     ),
                   ],
                 ),
                 child: IconButton(
-                  icon: Icon(Icons.close, color: Colors.red),
+                  icon: Icon(Icons.close, color: Color(0xFF3E0555)),
                   onPressed: widget.onPressed,
                 ),
               ),
@@ -55,9 +54,10 @@ class _ChatDialogState extends State<ChatDialog> {
             Text(
               'Chat Bot',
               style: TextStyle(
-                fontSize: 18,
+                fontSize: 24,
                 fontWeight: FontWeight.bold,
-                color: Colors.deepPurple,
+                color: Color(0xFF3E0555),
+                fontFamily: "REM",
               ),
             ),
             SizedBox(height: 16),
@@ -65,33 +65,39 @@ class _ChatDialogState extends State<ChatDialog> {
               child: Container(
                 width: double.infinity,
                 padding: EdgeInsets.all(12),
-                color: Colors.grey[200],
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                ),
                 child: SingleChildScrollView(
                   child: Text(
                     botResponse.isEmpty
                         ? "Press start to begin..."
                         : botResponse,
-                    style: TextStyle(fontSize: 16),
+                    style: TextStyle(fontSize: 16, fontFamily: "Poppins"),
                   ),
                 ),
               ),
             ),
             SizedBox(height: 12),
             ElevatedButton(
-              onPressed: isLoading
-                  ? null
-                  : () async {
-                      setState(() {
-                        isLoading = true;
-                        botResponse = "Loading...";
-                      });
-                      final service = HuggingFaceService();
-                      String reply = await service.getModelPrediction("start");
-                      setState(() {
-                        botResponse = reply;
-                        isLoading = false;
-                      });
-                    },
+              onPressed:
+                  isLoading
+                      ? null
+                      : () async {
+                        setState(() {
+                          isLoading = true;
+                          botResponse = "Loading...";
+                        });
+                        final service = HuggingFaceService();
+                        String reply = await service.getModelPrediction(
+                          "start",
+                        );
+                        setState(() {
+                          botResponse = reply;
+                          isLoading = false;
+                        });
+                      },
               style: ElevatedButton.styleFrom(
                 backgroundColor: Color(0xFF3E0555),
                 padding: EdgeInsets.symmetric(horizontal: 32, vertical: 12),
@@ -99,7 +105,14 @@ class _ChatDialogState extends State<ChatDialog> {
                   borderRadius: BorderRadius.circular(12),
                 ),
               ),
-              child: Text("Start", style: TextStyle(fontSize: 16)),
+              child: Text(
+                "Start",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                  fontFamily: "Poppins",
+                ),
+              ),
             ),
           ],
         ),
